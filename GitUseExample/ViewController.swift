c853124d10ee126f6390000b89accea5912b96bf
Anshu Shahi <anshu@dunzo.in>
2020-02-20 17:11:28 +0530

    First Commit

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonClicked(_ sender: UIButton) {
+      print("This is a Git tutorial")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
