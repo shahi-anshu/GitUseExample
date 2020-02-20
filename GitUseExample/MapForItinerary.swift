95361fe34b499463091c22f646097471aec336e0
Anshu Shahi <anshu@dunzo.in>
2020-02-20 17:20:52 +0530

    Add new map-for-itineary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Anshu Shahi on 20/02/20.
+//  Copyright © 2020 Anshu Shahi. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
